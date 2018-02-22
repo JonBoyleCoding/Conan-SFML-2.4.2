4522fbd66b0e0824d2a022ee17d9a3f79461cd55
Jonathan Boyle <programmerfalcon@gmail.com>
2018-02-22 22:13:31 +0000

    Initial Commit

---
+++ conanfile.py
@@ -0,0 +1,45 @@
+from conans import ConanFile, CMake, tools
+import os
+
+
+class SfmlConan(ConanFile):
+    name = "sfml"
+    version = "2.4.2"
+    license = "<Put the package license here>"
+    url = "<Package recipe repository url here, for issues about the package>"
+    description = "SFML is a simple, fast, cross-platform and object-oriented multimedia API. It provides access to windowing, graphics, audio and network. It is written in C++, and has bindings for various languages such as C, .Net, Ruby, Python."
+    settings = "os", "compiler", "build_type", "arch"
+    options = None
+    default_options = None
+    generators = "cmake"
+    
+    def requirements(self):
+        if self.settings.os == "Linux":
+            self.requires = ("openal/1.17.2@GatorQue/stable")
+
+    def source(self):
+        self.run("git clone https://github.com/SFML/SFML.git")
+        self.run("cd SFML && git checkout tags/2.4.2")
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure(source_folder="SFML")
+        cmake.definitions["BUILD_SHARED_LIBS"] = "ON"
+        cmake.definitions["CMAKE_INSTALL_PREFIX"] = "install"
+        cmake.install()
+
+    def package(self):
+        self.copy("*.h", dst="include", src="install/include")
+        self.copy("*.lib", dst="lib", src="install/", keep_path=False)
+        self.copy("*.dll", dst="bin", src="install/", keep_path=False)
+        self.copy("*.so", dst="lib", src="install/", keep_path=False)
+        self.copy("*.dylib", dst="lib", src="install/", keep_path=False)
+        self.copy("*.a", dst="lib", src="install/", keep_path=False)
+
+    def package_info(self):
+        self.cpp_info.libs = ["sfml-audio", "sfml-graphics", "sfml-network", "sfml-system", "sfml-window"]
+        
+        #self.cpp_info.libs.append("sfml-main")
+        
+        if self.settings.build_type == "Debug":
+            self.cpp_info.libs = [l + "-d" for l in self.cpp_info.libs]
